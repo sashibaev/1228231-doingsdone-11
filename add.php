d14deefe43e3518318403fa75e181413915edc46
Sergey Shibaev <srgjjshibaev@rambler.ru>
2019-11-19 22:43:42 +0300

    Поиск ошибки

--- add.php
+++ add.php
@@ -1,16 +1,16 @@
 <?php
-   include_once("../helpers.php");
+   include_once("helpers.php");
 
 
 
 
-   
+    
 
-$form_content = include_template("form-task.php", [
+    $form_content = include_template("form-task.php", [
         "user_name" => "Sergey",
         "title" => "Добавление задачи"
     ]);
 
 
-print($form_content);
+    print($form_content);
 ?>  
