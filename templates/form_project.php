aa2a8ff9227c69c02f58a98d0a25680626912bc5
Sergey Shibaev <srgjjshibaev@rambler.ru>
2019-12-16 17:42:15 +0300

    Доработка формы добавления проекта, form_project.php

--- templates/form_project.php
+++ templates/form_project.php
@@ -7,9 +7,14 @@
 
         <form class="form"  action="index.html" method="post" autocomplete="off">
           <div class="form__row">
+            <?php $classname = isset($errors["name"]) ? "form__input--error" : ""; ?>
             <label class="form__label" for="project_name">Название <sup>*</sup></label>
 
-            <input class="form__input" type="text" name="name" id="project_name" value="" placeholder="Введите название проекта">
+            <input class="form__input <?= $classname; ?>" type="text" name="name" id="project_name" value="
+              . <?=getPostVal("name"); ?>" placeholder="Введите название проекта">
+            <?php if (isset($errors["name"])): ?>
+              <p class = "form_message"><?=$errors["name"]; ?></p>
+            <?php endif; ?>  
           </div>
 
           <div class="form__row form__row--controls">
